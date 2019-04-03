e3fea753683f169273313ce83b24c40558a3d6e7
claresullivan <clares@gmail.com>
2019-04-03 12:17:56 -0500

    Activity 8 - Mapping

--- js/main.js
+++ js/main.js
@@ -1,163 +1,83 @@
 //execute script when window is loaded
-window.onload = function(){
-	//SVG dimension variables
-    var w = 900, h = 500;
+window.onload = fsetMap();
 
-    var container = d3.select("body") //get the <body> element from the DOM
-    	.append("svg") //put a new svg in the body
-    	.attr("width", w) //assign the width
-        .attr("height", h) //assign the height
-        .attr("class", "container") //always assign a class (as the block name) for styling and future selection
-    	.style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+//set up choropleth map
+function setMap(){
     
-    //innerRect block
-    var innerRect = container.append("rect") //put a new rect in the svg
-        .datum(400)
-        .attr("width", function(d){ //rectangle width
-            return d * 2; //400 * 2 = 800
-        }) 
-        .attr("height", function(d){ //rectangle height
-            return d; //400
-        })
-        .attr("class", "innerRect") //class name
-        .attr("x", 50) //position from left on the x (horizontal) axis
-        .attr("y", 50) //position from top on the y (vertical) axis
-        .style("fill", "#FFFFFF"); //fill color
+    //map frame dimensions
+    var width = 960,
+        height = 460;
 
-        //create data array
-        var cityPop = [
-        	{
-        		city: 'Leticia',
-        		population: 42000
-        	},
-        	{
-        		city: 'Florencia',
-        		population: 183323
-        	},
-        	{
-        		city: 'San Jose del Guaviare',
-        		population: 45705
-        	},
-        	{
-        		city: 'Puerto Asis',
-        		population: 33362
-        	}
-        	];
-        
-        //find the minimum value of the array
-        var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
 
-        //find the maximum value of the array
-        var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
+  //create Albers equal area conic projection centered on Colombia
+    var projection = d3.geoAlbers()
+        .center([6.910, -73.910])
+        .rotate([-2, 0, 0])
+        .parallels([43, 62])
+        .scale(2500)
+        .translate([width / 2, height / 2]);
 
-//scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50])
-        .domain([
-            0,
-            200000
-        ]);
+    var path = d3.geoPath()
+        .projection(projection);
+    
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];
+    promises.push(d3.csv("data/unitsData.csv")); //load attributes from csv
+    promises.push(d3.json("data/SouthAmerica.topojson")); //load background spatial data
+    promises.push(d3.json("data/ColombiaRegions.topojson")); //load choropleth spatial data
+    Promise.all(promises).then(callback);
 
-        //x coordinate linear scale
-        var x = d3.scaleLinear() //create the scale
-            .range([90, 810]) //output min and max
-            .domain([0, 3.5]); //input min and max
+    function callback(data){
+            //translate south america to TopoJSON
+        var SouthAmericaCountries = topojson.feature(SouthAmerica, SouthAmerica.objects.SouthAmericaCountries),
+            colombiaRegions = topojson.feature(Colombia, Colombia.objects.ColombiaRegions).features;
+    
+      //add South American countries to map
+        var countries = map.append("path")
+            .datum(SouthAmerica)
+            .attr("class", "countries")
+            .attr("d", path);
 
-        //color scale generator 
-        var color = d3.scaleLinear()
-            .range([
-            "#FDBE85",
-            "#D94701"
-            ])
-            .domain([
-                minPop, 
-                maxPop
-            ]);
-
-        var circles = container.selectAll(".circles") //but wait--there are no circles yet!
-            .data(cityPop) //here we feed in an array
-            .enter() //one of the great mysteries of the universe
-            .append("circle") //add a circle for each datum
-            .attr("class", "circles") //apply a class name to all circles
-            .attr("id", function(d){
-            return d.city;
-        })
-            .attr("r", function(d){ //circle radius
-             //calculate the radius based on population value as circle area
-        var area = d.population * 0.015;
-            return Math.sqrt(area/Math.PI);
-        })
-            .attr("cx", function(d, i){
-            //use the index to place each circle horizontally
-            return x(i);
-        })
-     	 .attr("cy", function(d){
-            return y(d.population);
-        })
-            .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000"); //black circle stroke
-
-        //need to call the style sheet for the axis style
-        //create y axis generator
-        var yAxis = d3.axisLeft(y);
-
-        //create axis g element and add axis
-        var axis = container.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(50, 0)")
-            .call(yAxis);
-
-        //adding a title
-        var title = container.append("text")
-            .attr("class", "title")
-            .attr("text-anchor", "middle")
-            .attr("x", 450)
-            .attr("y", 30)
-            .text("City Populations");
-
-        //create a label for the circles
-        var labels = container.selectAll(".labels")
-            .data(cityPop)
+        //add France regions to map
+        var regions = map.selectAll(".regions")
+            .data(colombiaRegions)
             .enter()
-            .append("text")
-            .attr("class", "labels")
-            .attr("text-anchor", "left")
-            .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population) + 5;
-            });
+            .append("path")
+            .attr("class", function(d){
+                return "regions " + d.properties.adm1_code;
+            })
+            .attr("d", path);
 
-        //first line of label
-        var nameLine = labels.append("tspan")
-            .attr("class", "nameLine")
-             .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
+                   //create graticule generator
+        var graticule = d3.geoGraticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+             
+
+            //create graticule background
+        var gratBackground = map.append("path")
+            .datum(graticule.outline()) //bind graticule background
+            .attr("class", "gratBackground") //assign class for styling
+            .attr("d", path) //project graticule
         
-        .text(function(d){
-            return d.city;
-        });
+        //create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
 
-        //create format generator
-        var format = d3.format(",");
-
-         //second line of label
-        var popLine = labels.append("tspan")
-            .attr("class", "popLine")
-            .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15") //vertical offset
-        .text(function(d){
-            return "Pop. " + format(d.population); //use format generator to format numbers
-        });
+    csvData = data[0];
+    SouthAmerica= data[1];
+    colombia = data[2];
+        console.log(csvData);
+        console.log(SouthAmerica);
+        console.log(colombia);
+    };
 };
-
